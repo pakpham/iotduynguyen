d9714ce9f21bc83dc5a59e128898d9e2ce013408
pakpham <pakpham@gmail.com>
2019-10-13 20:00:36 +0700

    update value now

---
+++ app/DataSensor3.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class DataSensor3 extends Model
+{
+    protected $table = 'data_sensor_3';
+}
---
+++ app/DataSensor4.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class DataSensor4 extends Model
+{
+    protected $table = 'data_sensor_4';
+}
